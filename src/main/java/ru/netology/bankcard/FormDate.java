0fe28e668d8c1f2285e01dbd526f4cc0946a53b7
Ivan <uzvano@mail.ru>
2024-07-22 20:41:22 +0300

    new project

---
+++ src/main/java/ru/netology/bankcard/FormDate.java
@@ -0,0 +1,7 @@
+package ru.netology.bankcard;
+
+public class FormDate {
+    public int day;
+    public int month;
+    public int year;
+}
---
+++ src/main/java/ru/netology/bankcard/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.bankcard;
+
+public class Post {
+    public FormDate birthday;
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public boolean subscription;
+}
